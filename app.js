3e5926cca88c4f91d46a0ef31fab48842975f255
g-sudhir <125783432+g-sudhir@users.noreply.github.com>
2023-05-05 12:11:49 +0530

    login page using node.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  // Add your logic to validate user credentials here
+
+  res.redirect('/dashboard');
+});
+app.listen(3000);
